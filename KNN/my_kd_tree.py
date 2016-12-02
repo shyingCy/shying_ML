0c2b82af2160a7a6af5b807af0abc40c90d38c38
745415234@qq.com <745415234@qq.com>
2016-12-02 18:02:06 +0800

    complete the knn in kd-tree with a bug

--- KNN/my_kd_tree.py
+++ KNN/my_kd_tree.py
@@ -2,10 +2,11 @@
 _author_ = "shying"
 
 import numpy as np
+import heapq as hq
 from ML_Util.distance_metric import get_distance
 
 class KD_node:
-    def __init__(self, point=None, split=None, LL=None, RR=None):
+    def __init__(self, point=None, split=None, LL=None, RR=None, PP = None):
         '''
 
         :param point:数据点
@@ -16,13 +17,14 @@ class KD_node:
         '''
         self.point = point
         self.split = split
+        self.parent = PP
         self.left = LL
         self.right = RR
 
 
 class KD_Tree:
     root = []
-
+    hp_knn = []
     def __init__(self, data=None):
         self.data = data
 
@@ -49,7 +51,11 @@ class KD_Tree:
         point = dataList[LEN/2]
         tree = KD_node(point, split)
         tree.left = self.build(tree.left, dataList[0: LEN/2])
+        if tree.left:
+            tree.left.parent = tree
         tree.right = self.build(tree.right, dataList[LEN/2 + 1: LEN])
+        if tree.right:
+            tree.right.parent = tree
         return tree
 
     def buildTree(self):
@@ -68,19 +74,66 @@ class KD_Tree:
     def preorder(self):
         self.tree_preorder(self.root)
 
+    def getLeaf(self, findPoint):
+        leaf = self.root
+        next = None
+        while leaf.left or leaf.right:
+            if findPoint[leaf.split] < leaf.point[leaf.split]:
+                next = leaf.left
+            elif findPoint[leaf.split] > leaf.point[leaf.split]:
+                next = leaf.right
+            else:
+                if get_distance(findPoint, leaf.left.point) < get_distance(findPoint, leaf.right.point):
+                    next = leaf.left
+                else:
+                    next = leaf.right
+
+            if next:
+                leaf = next
+
+        return leaf
+
+    def maintainHeap(self, Node, findPoint, k):
+
+        cur_dist = get_distance(Node.point, findPoint)
+        if len(self.hp_knn) < k:
+            hq.heappush(self.hp_knn, list(Node.point))
+        elif len(self.hp_knn) >= k and cur_dist < get_distance(findPoint, self.hp_knn[0]):
+            hq.heappop(self.hp_knn)
+            hq.heappush(self.hp_knn, list(Node.point))
+
+    def findKNN(self, findPoint, k):
+
+        nearestNode = self.getLeaf(findPoint)
+
+        while nearestNode:
+            cur_dist = get_distance(nearestNode.point, findPoint)
+            self.maintainHeap(nearestNode, findPoint, k)
+            if nearestNode.parent and cur_dist > abs(findPoint[nearestNode.parent.split] - nearestNode.parent.point[nearestNode.parent.split]):
+                if nearestNode.point[nearestNode.parent.split] <= nearestNode.parent.point[nearestNode.parent.split]:
+                    brotherNode = nearestNode.parent.right
+                else:
+                    brotherNode = nearestNode.parent.left
+                # 兄弟结点存在
+                if brotherNode:
+                    self.maintainHeap(brotherNode, findPoint, k)
+
+            nearestNode = nearestNode.parent
+        return
+
     # 返回距离findPoint最近的点以及距离
     def findNearest(self, findPoint):
 
         # NP存储离findPoint最近的点，min_dist存储最近距离
         NP = self.root.point
-        min_dist = get_distance(NP, findPoint)[0]
+        min_dist = get_distance(NP, findPoint)
 
         temp_root = self.root
         nodeList = []  # 存储经过的结点
         while temp_root:
 
             nodeList.append(temp_root)
-            dd = get_distance(temp_root.point, findPoint)[0]
+            dd = get_distance(temp_root.point, findPoint)
             if dd < min_dist:
                 NP = temp_root.point
                 min_dist = dd
@@ -103,7 +156,7 @@ class KD_Tree:
 
             if temp_root:
                 nodeList.append(temp_root)
-                dd = get_distance(findPoint, temp_root.point)[0]
+                dd = get_distance(findPoint, temp_root.point)
                 if dd < min_dist:
                     min_dist = dd
                     NP = temp_root.point
@@ -114,4 +167,7 @@ trainData = [[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]]
 t = KD_Tree(trainData)
 t.buildTree()
 t.preorder()
-print t.findNearest([2, 4.5])
+p = [2, 4.5]
+print t.findNearest(p)
+t.findKNN(p, 3)
+print t.hp_knn
